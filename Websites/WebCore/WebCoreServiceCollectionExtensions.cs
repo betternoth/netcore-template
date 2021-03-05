989e0b30387a8384d21812a7d173b91e04c0f4e9
Not Bancha <zzc0413426@celestica.com>
2021-03-05 16:13:09 +0700

    add shared ui web core

---
+++ Websites/WebCore/WebCoreServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCore
+{
+    public static class WebCoreServiceCollectionExtensions
+    {
+        public static void AddWebCore(this IServiceCollection services)
+        {
+            services.ConfigureOptions(typeof(WebCoreConfigureOptions));
+        }
+    }
+}
